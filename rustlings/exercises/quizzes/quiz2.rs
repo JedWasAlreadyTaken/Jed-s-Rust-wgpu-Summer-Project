00bd4db27250f3c20d018b12908289b2612a2c64
Jed <153555469+JedWasAlreadyTaken@users.noreply.github.com>
2026-07-19 23:30:24 +0100

    Hashmaps(no Ai)
    i need to withold my ai usage because this took me far too long to do on my own

--- rustlings/exercises/quizzes/quiz2.rs
+++ rustlings/exercises/quizzes/quiz2.rs
@@ -28,6 +28,24 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String>{
+       
+
+        let mut output: Vec<String> = vec![];
+        for (string, command) in &input { 
+          let transformed = match command{
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_owned(),
+                Command::Append(n) => {
+                    let repeated = "bar".repeat(*n);
+                format!("{}{}", string, repeated)
+                }
+            };
+            output.push(transformed) 
+        } 
+        output
+    } 
 }
 
 fn main() {
@@ -37,7 +55,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+     use crate::my_module::transformer;
     use super::Command;
 
     #[test]
